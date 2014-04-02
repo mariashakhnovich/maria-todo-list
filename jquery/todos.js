fdf7f7d2eba14e45a03dd87c5b341ae0653ef26a
mariashakhnovich <maria@symphonycommerce.com>
2014-04-02 16:50:52 -0700

    cleanup, underscores, exposing variables and functions to global scope

--- jquery/todos.js
+++ jquery/todos.js
@@ -1,4 +1,5 @@
  (function (exports) {
+
     var UI = {
          tasks: $("#tasks"),
          text: $("#txt"),
@@ -17,6 +18,7 @@
          this.text = text;
          this.done = done;
          this.checked = "";
+         this.deleted = false;
      };
 
      function addNewTask() {
@@ -27,29 +29,19 @@
          if (newTask.text !== "") {
              toDoList.push(newTask);
              UI.text.val("");
-         } 
-
+         }
          render(toDoList);
      }
 
      function selectAll() {
-        if (toDoList.length === 0) {
-            var trueOrFalse = true;
-        } else {
-            var trueOrFalse = !toDoList[0].done;
-        }
 
-        if (trueOrFalse) {
-            var isChecked = "checked";
-        } else {
-            var isChecked = "";
-        }
+        var isSelected = (toDoList.length === 0 || !toDoList[0].done);  
+        var isChecked = isSelected ? "checked" : "" 
 
-        for (var i = 0; i<toDoList.length; i++) {
-            toDoList[i].done = trueOrFalse;
-            toDoList[i].checked = isChecked;
-        }
-
+        $.each(toDoList, function(i, val){
+            val.done = isSelected;
+            val.checked = isChecked;
+        })
         render(toDoList);  
      }
 
@@ -58,32 +50,25 @@
             val.done = false;
             val.deleted = true;
         })
-
         render(toDoList);
      } 
 
      function getDoneTasks(list) {
-        var doneTasks = [];
-        $.each(toDoList, function(i, val){
-            if (val.done && !val.deleted) {
-                doneTasks.push(val);
-            }
-        })
+        var doneTasks = _.where(list, {done: true, deleted: false}); //don't care about deleted, only need "done"
         return doneTasks;  
      }
 
-     function render(list) {
-         var progressHelperMessage;
-         var progressMessageClassName;
-         var hbrsText = todoTemplate(data);
-         var tasksDone = getDoneTasks(list);
-         var totalTasks = 0;
+     function getDeletedTasks(list) {
+        var deletedTasks = _.where(list, {deleted: true});
+        return deletedTasks;
+     }
 
-        $.each(list, function(i, val) {
-            if(!val.deleted){
-                totalTasks++;
-            }
-        })
+     function render(list) {
+        var progressHelperMessage;
+        var progressMessageClassName;
+        var hbrsText = todoTemplate(data);
+        var tasksDone = getDoneTasks(list);
+        var totalTasks = list.length - getDeletedTasks(list).length;
 
         if (totalTasks === 0) {
              progressMessageClassName = "emptyList";
@@ -91,25 +76,19 @@
         } else {
              progressHelperMessage = "Done: " + tasksDone.length + " out of " + totalTasks;
              progressMessageClassName = "inList";
-         }
+        }
 
-         UI.total.text(progressHelperMessage).removeClass().addClass(progressMessageClassName);
-         UI.tasks.html(hbrsText);
+        UI.total.text(progressHelperMessage).removeClass().addClass(progressMessageClassName);
+        UI.tasks.html(hbrsText);
      }
 
      UI.tasks.on("click", "input", function(event) {
         var todoId = $(this).closest("div").data("id");
         toDoList[todoId].done = !toDoList[todoId].done;
-        
-        if (toDoList[todoId].done) {
-            toDoList[todoId].checked = "checked";
-        } else {
-            toDoList[todoId].checked = "";
-        }
-
+        toDoList[todoId].checked = toDoList[todoId].done ? "checked" : "";
         render(toDoList);
         return false;
-    }).on("click", ".delete", function(event) {
+     }).on("click", ".delete", function(event) {
         var todoId = $(this).closest("div").data("id");
         $.each(toDoList, function(i, val){
             if (val.id ===todoId) {
@@ -123,4 +102,15 @@
      UI.addBtn.click(addNewTask);
      UI.selectAllBtn.click(selectAll);
      UI.deleteDoneBtn.click(deleteDone);
- }(window));
+
+     window.exports = {
+        toDoList: toDoList,
+        addNewTask: addNewTask,
+        selectAll: selectAll,
+        deleteDone: deleteDone,
+        getDeletedTasks: getDeletedTasks,
+        getDoneTasks: getDoneTasks,
+        render: render
+     };
+
+ }(window.jQuery));
