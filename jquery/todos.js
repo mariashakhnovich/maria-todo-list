194683c45f83c369a48903eae69f599b7553950a
mariashakhnovich <maria@symphonycommerce.com>
2014-04-07 13:09:27 -0700

    cleaned up render and selectAll methods

--- jquery/todos.js
+++ jquery/todos.js
@@ -12,7 +12,7 @@
 
      var todoTemplate = Handlebars.compile(UI.todos.html());   
      var toDoList = [];
-     var data = {"tasks": toDoList };
+     var data = {"tasks": toDoList, doneTasks:0, totalTasks: 0, summaryClass:"emptyList", summaryMessage: "Enter a task!" };
      var Task = function (id, text, done) {
          this.id = id;
          this.text = text;
@@ -21,29 +21,32 @@
          this.deleted = false;
      };
 
-     function addNewTask() {
-         var text = UI.text.val();
+     function addNewTask(text) {
+         var text = UI.text.val() || text;
          var done = false;
          var id = toDoList.length;
          var newTask = new Task(id, text, done);
-         if (newTask.text !== "") {
+         if (newTask.text !== "" && jQuery.type(newTask.text) === "string" ) {
              toDoList.push(newTask);
              UI.text.val("");
          }
          render(toDoList);
      }
 
-     function selectAll() {
-
+     function selectOrClearAll() {
         var isSelected = (toDoList.length === 0 || !toDoList[0].done);  
-        var isChecked = isSelected ? "checked" : "" 
+        selectAll(isSelected);  
+     }
 
+     function selectAll(isSelected) {
+        var isChecked = isSelected ? "checked" : ""
         $.each(toDoList, function(i, val){
             val.done = isSelected;
             val.checked = isChecked;
         })
-        render(toDoList);  
+        render(toDoList); 
      }
+
 
      function deleteDone() {
         $.each(getDoneTasks(toDoList), function(i, val){
@@ -54,32 +57,19 @@
      } 
 
      function getDoneTasks(list) {
-        var doneTasks = _.where(list, {done: true, deleted: false}); //don't care about deleted, only need "done"
-        return doneTasks;  
+        return _.where(list, {done: true, deleted: false});
      }
 
      function getDeletedTasks(list) {
-        var deletedTasks = _.where(list, {deleted: true});
-        return deletedTasks;
+        return _.where(list, {deleted: true});
      }
 
-     function render(list) {
-        var progressHelperMessage;
-        var progressMessageClassName;
-        var hbrsText = todoTemplate(data);
-        var tasksDone = getDoneTasks(list);
-        var totalTasks = list.length - getDeletedTasks(list).length;
-
-        if (totalTasks === 0) {
-             progressMessageClassName = "emptyList";
-             progressHelperMessage = "Enter a task!";
-        } else {
-             progressHelperMessage = "Done: " + tasksDone.length + " out of " + totalTasks;
-             progressMessageClassName = "inList";
-        }
-
-        UI.total.text(progressHelperMessage).removeClass().addClass(progressMessageClassName);
-        UI.tasks.html(hbrsText);
+     function render(list) { 
+        data.doneTasks = getDoneTasks(list).length;
+        data.totalTasks = list.length - getDeletedTasks(list).length;
+        data.summaryMessage = (data.doneTasks === 0 && data.totalTasks === 0) ? "Enter a task!" : "Done " + data.doneTasks + " out of "+ data.totalTasks;
+        data.summaryClass = data.summaryMessage === "Enter a task!" ? "emptyList" : "";
+        UI.tasks.html(todoTemplate(data));
      }
 
      UI.tasks.on("click", "input", function(event) {
@@ -90,17 +80,15 @@
         return false;
      }).on("click", ".delete", function(event) {
         var todoId = $(this).closest("div").data("id");
-        $.each(toDoList, function(i, val){
-            if (val.id ===todoId) {
-                val.deleted = true;
-                render(toDoList);
-            }
-        })
+        var toDelete = _.findWhere(toDoList, {id: todoId});
+        toDelete.deleted = true;
+        render(toDoList);
         return false;
      });
 
+     render(toDoList);
      UI.addBtn.click(addNewTask);
-     UI.selectAllBtn.click(selectAll);
+     UI.selectAllBtn.click(selectOrClearAll);
      UI.deleteDoneBtn.click(deleteDone);
 
      window.exports = {
@@ -109,8 +97,9 @@
         selectAll: selectAll,
         deleteDone: deleteDone,
         getDeletedTasks: getDeletedTasks,
-        getDoneTasks: getDoneTasks,
-        render: render
+        getDoneTasks: getDoneTasks
      };
 
  }(window.jQuery));
+
+ //write tests for each function - jasmine
