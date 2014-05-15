4fb756c59d5b7c80acaaa744e0345143bcf723a1
mariashakhnovich <maria@symphonycommerce.com>
2014-05-15 11:31:58 -0700

    fixed summary message, counts, added select/clear toggle, deleteDone, fixed deleteTask.

--- angular/todosang.js
+++ angular/todosang.js
@@ -1,20 +1,42 @@
 var myApp = angular.module('todos', []);
 myApp.controller('MainCtrl', ['$scope', function ($scope) {
-	$scope.summaryMessage = "Enter a task!";
 	$scope.toDoList = [];
 
 	$scope.addNewTask = function() {
     	if ($scope.todoText !== "") {
     		$scope.toDoList.push({text:$scope.todoText, done:false});
-    		$scope.summaryMessage = "Enter a task!";
-    	}
-    	else{
-    		$scope.summaryMessage = "Task cannot be empty!";
-    	}
+    	};
     	$scope.todoText = '';
   	};
 
-  	//$scope.summaryMessage = (temp === 0 && toDoList.length === 0) ? "Enter a task!" : "Done " + data.doneTasks + " out of "+ data.totalTasks;
-    //$scope.summaryClass = data.summaryMessage === "Enter a task!" ? "emptyList" : "";
+    $scope.deleteTask = function(task){
+        $scope.toDoList.splice($scope.toDoList.indexOf(task),1);
+    };
 
+    $scope.deleteDone = function(list) {
+        var doneTasks = $scope.getDoneTasks(list);
+        for (var i=0; i<doneTasks.length; i++ ){
+            $scope.deleteTask(doneTasks[i]);
+        }
+    };
+
+    $scope.toggleSelect = function(list) {
+        var selection = (list.length === 0 || !list[0].done);
+        $scope.changeSelection(list, selection);
+    };
+
+    $scope.changeSelection = function(list, selection) {
+        angular.forEach(list, function(value, key){
+            value.done = selection;
+        })
+    }
+    
+    $scope.getDoneTasks = function(list){
+        return _.where(list, {done: true});
+    }
+
+    $scope.getDoneTasksCount = function(list){
+        var doneTasks = _.where(list, {done: true});
+        return doneTasks.length;
+    }  
 }]);
